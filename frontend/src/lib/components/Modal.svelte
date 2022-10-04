e042d79367706d35ef3ac8b315172198e2e3acdc
Srujan Gurram <srujangs8@gmail.com>
2022-10-04 12:06:15 +0530

    added model for transaction details

---
+++ frontend/src/lib/components/Modal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let open: boolean = false;
+  export let title: string = "";
+
+  import { onMount } from "svelte";
+
+  let toggle: HTMLInputElement;
+  onMount(() => {
+    if (open) toggle.checked = open;
+    else toggle.checked = false;
+  });
+
+  $: if (open && toggle) toggle.checked = open;
+</script>
+
+<input type="checkbox" hidden bind:this={toggle} class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">{title}</h3>
+    <div class="py-4">
+      <slot />
+    </div>
+  </div>
+</div>
--- frontend/src/routes/add-record/+page.svelte
+++ frontend/src/routes/add-record/+page.svelte
@@ -4,8 +4,14 @@
   import type { RecordDetails } from "$lib/types";
   import { contracts } from "svelte-ethers-store";
   import { toast } from "$lib/store/toast";
+  import Modal from "$lib/components/Modal.svelte";
+  import { BigNumber, type ContractReceipt } from "ethers";
 
   $: connectionGuard();
+
+  let loading = false;
+  let transactionResult: any = false;
+  let isModalOpen = false;
 
   const handleAddRecordSubmit = async (e: Event) => {
     e.preventDefault();
@@ -13,22 +19,78 @@
     const payload = Object.fromEntries(formData.entries()) as RecordDetails;
 
     attachRecordContract();
+    loading = true;
 
     try {
       const response = await $contracts.recordsContract.addRecord(
         payload.name,
         payload.description
       );
-      const data = await response.wait();
+      transactionResult = await response.wait();
+      isModalOpen = true;
     } catch (e: any) {
       toast({
         message: e.message,
         type: "error",
       });
     }
+    loading = false;
   };
 </script>
 
+{#if transactionResult}
+  <Modal open={isModalOpen} title="✅ Transaction Succesfull">
+    <div class="overflow-x-auto">
+      <table class="table w-full table-compact">
+        <tbody>
+          <tr>
+            <th> Transaction Hash:</th>
+            <td>{transactionResult?.transactionHash}</td>
+          </tr>
+          <!-- row 2 -->
+          <tr>
+            <th>Block Number</th>
+            <td>{transactionResult?.blockNumber}</td>
+          </tr>
+          <!-- row 3 -->
+          <tr>
+            <th>Record ID</th>
+            <td
+              >{BigNumber.from(
+                transactionResult?.events?.[0].args?.[0]
+              ).toString()}</td
+            >
+          </tr>
+          <!-- row 4 -->
+          <tr>
+            <th>Record Name</th>
+            <td>{transactionResult?.events?.[0].args?.[1]}</td>
+          </tr>
+          <!-- row 5 -->
+          <tr>
+            <th>Record Description</th>
+            <td>{transactionResult?.events?.[0].args?.[2]}</td>
+          </tr>
+
+          <!-- row 6 -->
+          <tr>
+            <th>Record Maintainer</th>
+            <td>{transactionResult?.events?.[0].args?.[3]}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="modal-action">
+      <label
+        on:click={() => {
+          isModalOpen = false;
+        }}
+        for="my-modal-6"
+        class="btn">Yay!</label
+      >
+    </div>
+  </Modal>
+{/if}
 <div class="grid place-content-center min-h-screen">
   <div class=" flex flex-col gap-6 max-w-md">
     <h1 class="font-extrabold text-5xl">Add New Record</h1>
@@ -55,7 +117,13 @@
           class="textarea textarea-bordered w-full"
         />
       </div>
-      <button class="btn btn-primary mt-4 w-full">Add Record</button>
+      {#if !loading}
+        <button class="btn btn-primary mt-4 w-full">Add Record</button>
+      {:else}
+        <button class="btn loading btn-primary mt-4 w-full" disabled
+          >Transaction Processing</button
+        >
+      {/if}
     </form>
   </div>
 </div>
