5a6823b7f659d0ae87dff4e66b556c885d18b695
Srujan Gurram <srujangs8@gmail.com>
2022-10-09 19:50:09 +0530

    added nprogress

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,22 @@
   import "../app.css";
   import { toastData } from "$lib/store/toast";
   import Toast from "$lib/components/elements/Toast.svelte";
+  //@ts-ignore
+  import NProgress from "nprogress";
+  import { navigating } from "$app/stores";
+
+  NProgress.configure({
+    minimum: 0.16,
+  });
+
+  $: {
+    if ($navigating) {
+      NProgress.start();
+    }
+    if (!$navigating) {
+      NProgress.done();
+    }
+  }
 </script>
 
 <div class="max-w-7xl mx-auto px-5">
