10f9e12147dab0bbbeb2864b3ede61e7cca8c8a2
Srujan Gurram <srujangs8@gmail.com>
2022-11-13 22:00:47 +0530

    increased height of entries table skeleton

--- frontend/src/lib/components/skeletons/SkeletonEntriesTable.svelte
+++ frontend/src/lib/components/skeletons/SkeletonEntriesTable.svelte
@@ -6,7 +6,7 @@
   </div>
   <div class="flex gap-4 mt-4">
     {#each Array(4) as _}
-      <div class="h-72 w-[calc(100vw/4)] bg-base-300 rounded" />
+      <div class="h-80 w-[calc(100vw/4)] bg-base-300 rounded" />
     {/each}
   </div>
 </div>
