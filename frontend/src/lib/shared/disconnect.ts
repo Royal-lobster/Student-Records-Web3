9b08b23b5dcf11a5ddddf753753be1501492737e
Srujan Gurram <srujangs8@gmail.com>
2022-11-15 13:02:01 +0530

    made entire local storage to clear after disconnect

--- frontend/src/lib/shared/disconnect.ts
+++ frontend/src/lib/shared/disconnect.ts
@@ -1,8 +1,8 @@
 import { defaultEvmStores } from "svelte-ethers-store";
-import { web3auth, web3authStore } from "./connect";
+import { web3authStore } from "./connect";
 
 export const disconnect = async () => {
-  localStorage.removeItem("connected");
+  localStorage.clear();
   defaultEvmStores.disconnect();
   web3authStore.subscribe((c) => c?.logout())();
   window.location.reload();
