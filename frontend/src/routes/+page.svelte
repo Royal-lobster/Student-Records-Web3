ef19880cc40b4a2379f709b709c3fcfa0ed722c1
Srujan Gurram <srujangs8@gmail.com>
2023-03-30 13:16:42 +0530

    Add a JNTU logo and a header to the page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,7 +6,18 @@
 </script>
 
 <div>
-  <Navbar name="Records Web3" />
+  <nav
+    class="flex flex-col justify-center flex-wrap items-center w-full mb-16 mt-10 border-b-[1px] border-black/10 pb-4"
+  >
+    <img src="/images/jntugv-logo.svg" alt="logo" width="45px" />
+    <h2 class="text-lg text-center md:text-2xl">
+      Jawaharlal Nehru Technological University - Gurajada, Vizianagaram
+    </h2>
+    <h1 class="text-lg text-center md:text-2xl text-neutral">
+      Student Record Management System
+    </h1>
+  </nav>
+
   <Hero />
   <Concept />
   <UsesAndLimitations />
